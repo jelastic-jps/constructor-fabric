955b4e3e87535a8eb718e43a09841e7e4ad749e6
Ihor Kolodyuk <ihor.kolodyuk@virtuozzo.com>
2026-05-29 14:05:59 +0300

    Remove invalid x11vnc clipboard flag

--- patch-x11vnc.py
+++ patch-x11vnc.py
@@ -4,11 +4,11 @@ if p.exists():
     s = p.read_text()
     s = s.replace(
         'command=x11vnc -display :1 -xkb -forever -shared -repeat -noxfixes -noxdamage -nowf -noscr -listen 0.0.0.0 -rfbport 5900 -rfbauth /.password2',
-        'command=x11vnc -display :1 -xkb -forever -shared -repeat -noxfixes -noxdamage -nowf -noscr -listen 0.0.0.0 -rfbport 5900 -rfbauth /.password2 -clipboard'
+        'command=x11vnc -display :1 -xkb -forever -shared -repeat -noxfixes -noxdamage -nowf -noscr -listen 0.0.0.0 -rfbport 5900 -rfbauth /.password2'
     )
     s = s.replace(
         'command=x11vnc -display :1 -xkb -forever -shared -repeat -rfbauth /.password2',
-        'command=x11vnc -display :1 -xkb -forever -shared -repeat -rfbauth /.password2 -clipboard'
+        'command=x11vnc -display :1 -xkb -forever -shared -repeat -rfbauth /.password2'
     )
     p.write_text(s)
-    print('x11vnc supervisor patched with -clipboard')
+    print('x11vnc supervisor patched for native VNC')
